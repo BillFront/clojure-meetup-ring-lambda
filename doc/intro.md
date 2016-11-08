5adef9f597f2968bc7388a2c35c4f6fbf6564318
Marek Nowak <mareknowakk@gmail.com>
2016-11-08 13:16:19 +0100

    Inits.

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to coolest-animal-ring-lambda
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/coolest_animal_ring_lambda/core_test.clj
@@ -0,0 +1,7 @@
+(ns coolest-animal-ring-lambda.core-test
+  (:require [clojure.test :refer :all]
+            [coolest-animal-ring-lambda.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
