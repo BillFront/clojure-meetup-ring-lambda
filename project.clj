fb5279a5612dfa9ebaa6c8a8c172c2c66a7dd69a
Marek Nowak <mareknowakk@gmail.com>
2016-11-08 14:12:08 +0100

    Implements coolest animal over ring.

--- project.clj
+++ project.clj
@@ -3,7 +3,12 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.8.0"]]
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [digest "1.4.5"]
+                 [ring/ring-core "1.5.0"]
+                 [ring/ring-jetty-adapter "1.5.0"]]
+  :plugins [[lein-ring "0.9.7"]]
+  :ring {:handler coolest-animal-ring-lambda.core/handler}
   :main ^:skip-aot coolest-animal-ring-lambda.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
--- src/coolest_animal_ring_lambda/core.clj
+++ src/coolest_animal_ring_lambda/core.clj
@@ -1,7 +1,18 @@
 (ns coolest-animal-ring-lambda.core
+  (:require [digest :refer [md5]])
   (:gen-class))
 
-(defn -main
-  "I don't do a whole lot ... yet."
-  [& args]
-  (println "Hello, World!"))
+(defn- cool-factor [animal] (->> animal
+                                 md5
+                                 char-array
+                                 (map int)
+                                 (reduce +)))
+
+(defn- coolest [animals] (->> animals
+                              (sort-by cool-factor)
+                              last))
+
+(defn handler [request]
+  (let [body (-> request :body slurp)
+        animals (clojure.string/split body #",")]
+    {:body (coolest animals)}))
